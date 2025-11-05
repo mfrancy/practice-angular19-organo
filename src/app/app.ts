7606c16f72c1a3adcee5a777994bccfea12a899f
Mariana <marianalopes697@gmail.com>
2025-11-05 10:24:35 -0300

    feat: adicionando componente footer na página principal

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,13 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { Cabecalhos } from "./components/cabecalhos/cabecalhos";
+import { Cabecalhos } from './components/cabecalhos/cabecalhos';
+import { Footer } from './components/footer/footer';
 
 @Component({
   selector: 'app-root',
-  imports: [Cabecalhos],
+  imports: [Cabecalhos, Footer],
   templateUrl: './app.html',
-  styleUrl: './app.css'
+  styleUrl: './app.css',
 })
 export class App {
   protected readonly title = signal('organo');
